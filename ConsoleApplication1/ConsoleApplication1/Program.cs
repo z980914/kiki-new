d7a3617cb0803c243c75fba718173ce43d6e16c6
m305-21\w401 <1604314896@qq.com>
2018-03-22 17:00:42 +0800

    di er ge

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,17 +11,21 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Random rand = new Random();
-
-            int r1 = rand.Next(1, 43);
-            int r2 = rand.Next(1, 43);
-            int r3 = rand.Next(1, 43);
-            int r4 = rand.Next(1, 43);
-            int r5 = rand.Next(1, 43);
-            int r6 = rand.Next(1, 43);
-
-            //xuan gao bian shu
-
-            Console.WriteLine("{0:0} {1:0} {2:0} {3:0} {4:0} {5:0}", r1, r2, r3, r4, r5, r6);
+            int n = int .Parse (Console .ReadLine());
+            int a=0;
+            while (a <n)
+            {
+                a  += 1;
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
+                Console.WriteLine("{0:0} {1:0} {2:0} {3:0} {4:0} {5:0}", r1, r2, r3, r4, r5, r6);
+            }
+              
+          
 
             //suijibianshu
             Console.ReadLine();
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] heights = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 170, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 0, 0, 0, 168, 160, 169, 0, 150 };
+            Random rand = new Random();
+            int r = rand.Next(0, heights.Length);
+            Console.WriteLine(r + ":" + heights[r]);
+            Console.ReadLine();
+
+        }
+    }
+}
