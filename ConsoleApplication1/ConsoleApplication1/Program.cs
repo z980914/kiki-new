1b8330be6c55435821ef04f935673f63a66d4e60
m305-21\w401 <1604314896@qq.com>
2018-03-22 15:49:19 +0800

    zuoye

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            rand.Next(0, 100);
+            Console.ReadLine();
+        }
+    }
+}
